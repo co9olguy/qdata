744baef272f6c84a62bab1d2bc263b163315567d
Josh Izaac <josh146@gmail.com>
2020-10-27 21:02:42 +0800

    black the setup.py file

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ requirements = [
 ]
 
 info = {
-    'name': 'qdata',
-    'version': version,
-    'maintainer': 'Nathan Killoran',
-    'url': 'https://github.com/co9olguy/qdata',
-    'packages': find_packages(where="."),
-    'description': 'An extension of OpenQASM which aims to add operators.',
-    'long_description': open('README.md').read(),
-    'long_description_content_type': "text/x-md",
-    'provides': ["qdata"],
-    'install_requires': requirements,
-    'package_data': {'qdata': ['qasm.lark', 'include/*']},
-    'include_package_data': True
+    "name": "qdata",
+    "version": version,
+    "maintainer": "Nathan Killoran",
+    "url": "https://github.com/co9olguy/qdata",
+    "packages": find_packages(where="."),
+    "description": "An extension of OpenQASM which aims to add operators.",
+    "long_description": open("README.md").read(),
+    "long_description_content_type": "text/x-md",
+    "provides": ["qdata"],
+    "install_requires": requirements,
+    "package_data": {"qdata": ["qasm.lark", "include/*"]},
+    "include_package_data": True,
 }
 
 classifiers = [
@@ -34,12 +34,12 @@ classifiers = [
     "Operating System :: POSIX :: Linux",
     "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3 :: Only',
-    "Topic :: Scientific/Engineering :: Physics"
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3 :: Only",
+    "Topic :: Scientific/Engineering :: Physics",
 ]
 
 setup(classifiers=classifiers, **(info))

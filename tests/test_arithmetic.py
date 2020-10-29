8f517de592ad73676f6d7a44ce881fb4ad1d8af5
rooler <roel.wier@gmail.com>
2020-10-29 10:11:46 -0400

    Fixed imports, changed Op to operation, fixed test_instantiate_arbitrary_operation

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -2,30 +2,29 @@ import sys
 
 import pytest
 
-import parser
-
+from qdata.ir import *
 
 def test_instantiate_arbitrary_operation(capsys):
     """Test that instantiating an arbitrary arithmetic operation works
     correctly"""
 
-    op = parser.ArithmeticOp("func", 1, 2, 3, 4)
-    assert not isinstance(op, (parser.BinaryOp, parser.UnaryOp))
+    op = ArithmeticOperation("func", 1, 2, 3, 4)
+    assert not isinstance(op, (BinaryOperation, UnaryOperation))
     assert op.func == "func"
     assert op.args == (1, 2, 3, 4)
     assert op.tuple == ("func", 1, 2, 3, 4)
 
     print(op)
     captured = capsys.readouterr()
-    assert captured.out == "ArithmeticOp(func=func, exps=(1, 2, 3, 4))\n"
+    assert captured.out == "ArithmeticOperation(func=func, args=(1, 2, 3, 4))\n"
 
 
 def test_instantiate_unary_operation(capsys):
     """Test that instantiating a unary arithmetic operation works
     correctly"""
 
-    op = parser.ArithmeticOp("func", 1)
-    assert isinstance(op, parser.UnaryOp)
+    op = ArithmeticOperation("func", 1)
+    assert isinstance(op, UnaryOperation)
 
     print(op)
     captured = capsys.readouterr()
@@ -36,8 +35,8 @@ def test_instantiate_binary_operation(capsys):
     """Test that instantiating a binary arithmetic operation works
     correctly"""
 
-    op = parser.ArithmeticOp("func", 1, 2)
-    assert isinstance(op, parser.BinaryOp)
+    op = ArithmeticOperation("func", 1, 2)
+    assert isinstance(op, BinaryOperation)
 
     print(op)
     captured = capsys.readouterr()
@@ -45,9 +44,9 @@ def test_instantiate_binary_operation(capsys):
 
 
 arithmetic_arg_data = [
-    [parser.ArithmeticOp("func", 1, 2), 2],
-    [0.3, parser.ArithmeticOp("func", 1, 2)],
-    [parser.ArithmeticOp("func1", 1, 2), parser.ArithmeticOp("func2", 1, 2)],
+    [ArithmeticOperation("func", 1, 2), 2],
+    [0.3, ArithmeticOperation("func", 1, 2)],
+    [ArithmeticOperation("func1", 1, 2), ArithmeticOperation("func2", 1, 2)],
 ]
 
 
@@ -77,8 +76,8 @@ def test_division(arg1, arg2):
 
 @pytest.mark.parametrize("arg1,arg2", arithmetic_arg_data)
 def test_negation(arg1, arg2):
-    if not isinstance(arg1, parser.ArithmeticOp):
-        pytest.skip("Test only supports ArithmeticOp arguments")
+    if not isinstance(arg1, ArithmeticOperation):
+        pytest.skip("Test only supports ArithmeticOperation arguments")
 
     op2 = - arg1
     assert op2.func == "neg"
